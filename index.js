71a02dbb56803bfc9770d7a909271a0a51fa7610
jmendez44 <jenuel.mendez@gmail.com>
2020-05-05 01:14:52 -0400

    heroku fixed

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const resolvers = require("./resolvers/resolvers");
 const typeDefs = require("./schemas/typeDefs");
 require("dotenv").config();
-// const db = mongoose.connection;
 
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const db = mongoose.connection;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const server = new ApolloServer({
   typeDefs,
@@ -30,14 +31,13 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", function () {
-//   console.log("db connected");
-
-// });
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("db connected");
   app
     .listen({
       port: process.env.PORT || 5000,
     } , () => {
       console.log(`Server started at http://localhost:${process.env.PORT}`)
     })
+});
