131a37046516dfae606badc3b4a0029a7105d6c6
jmendez44 <jenuel.mendez@gmail.com>
2020-06-24 02:45:58 -0400

    server setup

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
-const { ApolloServer } = require("apollo-server");
-const color = require("colors");
-const mongoose = require("mongoose");
-const resolvers = require("./resolvers/resolvers");
-const typeDefs = require("./schemas/typeDefs");
+const express = require("express");
 require("dotenv").config();
-const uristring = process.env.MONGO_URI;
+const app = express();
 
-mongoose.connect(uristring, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
+app.get("/", (req, res) => {
+  res.send("this is the server");
 });
 
-const db = mongoose.connection;
+// app.post('/login', passport.authenticate('local'), () => {
+//   res.redirect('http://localhost:3000/dashboard')
+// })
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  //Add context like usernames and 0authentication. passes through resolvers as context etc....
-  context: ({ req }) => {
-    const fakeUser = {
-      userId: "lotso",
-    };
-    return {
-      ...fakeUser,
-    };
-  },
-  introspection: true,
-  playground: true,
-});
 
-// server.applyMiddleware({ });
-
-db.on("error", console.error.bind(console, "connection error:".bgRed));
-db.once("open", function () {
-  console.log("✅ db connected ✅".rainbow);
-  server
-    .listen({
-      port: process.env.PORT || 5000,
-    })
-    .then(({ url }) => {
-      console.log(`Server started at ${url}`);
-    });
+app.listen(4000, () => {
+  console.log("listening on port http://localhost:4000/");
 });
--- schemas/userSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Topic } = require("../schemas/topicSchema");
-
-const User = new mongoose.Schema({
-  userId: String,
-  topics: [Topic],
-});
-
-module.exports = mongoose.model("User", User);
